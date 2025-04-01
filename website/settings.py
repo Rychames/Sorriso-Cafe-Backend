58745362cf4b84488841b16766ef1cd6679d9e36
rychames <rychames@gmail.com>
2025-04-01 15:57:59 -0400

    fix: docker

--- website/settings.py
+++ website/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["ppscanner-dev-backend-production.up.railway.app", 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
